9c425d23cc847f39ade2aa40669ae38313b32eea
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2024-02-15 22:38:43 +1300

    Added first section to games pages

--- src/app/games/layout.tsx
+++ src/app/games/layout.tsx
@@ -1,11 +1,14 @@
+import Image from 'next/image'
 import React from 'react'
+import background from '../../../public/images/game-background.png'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      {/* <Image></Image> */}
-
-      {children}
+    <div className="">
+      <div className="absolute right-0 w-[40%] lg:w-fit -z-10">
+        <Image quality={100} src={background} alt={'background'}></Image>
+      </div>
+      <div className="p-10">{children}</div>
     </div>
   )
 }
--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,3 +1,32 @@
+import Image from 'next/image'
+
+import charactors from '../../../public/images/game-characters.png'
+import logo from '../../../public/images/game-logo.png'
+
 export default function Page() {
-  return <></>
+  return (
+    <>
+      <section className="flex justify-between items-center">
+        <div>
+          <br />
+          <div className="w-[350px]">
+            <Image quality={100} src={logo} alt="game-characters"></Image>
+          </div>
+          <br />
+          <p className="max-w-[500px] text-lg">
+            Play, laugh, and learn! HEIHEI tākaro is a safe place for our
+            tamariki. Watch shows on TVNZ On Demand, or Play games right here.
+          </p>
+        </div>
+        <div className="mr-44 my-16 ">
+          <Image
+            quality={75}
+            src={charactors}
+            alt="game-characters"
+            width={400}
+          ></Image>
+        </div>
+      </section>
+    </>
+  )
 }
