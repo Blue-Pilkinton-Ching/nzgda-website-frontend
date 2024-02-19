60c8a55427979e8b7cc7ce486d954afc554d5d64
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2024-02-19 14:33:55 +1300

    Added min height to pages

--- src/app/games/layout.tsx
+++ src/app/games/layout.tsx
@@ -5,12 +5,12 @@ import Footer from '../footer'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="">
+    <>
       <div className="absolute right-0 w-[40%] lg:w-fit -z-10">
         <Image quality={100} src={background} alt={'background'}></Image>
       </div>
-      <div className="p-10">{children}</div>
+      <div className="p-10 min-h-svh">{children}</div>
       <Footer />
-    </div>
+    </>
   )
 }
