55d8eae94d27df91885bd1f85233035213720157
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2024-02-20 14:53:19 +1300

    Added contact us page

--- src/app/about/contact/page.tsx
+++ src/app/about/contact/page.tsx
@@ -1,51 +1,21 @@
+import H1 from '../H1'
+import H2 from '../H2'
+import P from '../P'
+import About from '../about'
+
 export default function Page() {
   return (
     <>
-      <h1>The Who, What and Why</h1>
-      <h2>Who is HEIHEI?</h2>
-      <p>
-        HEIHEI is brought to you by a mighty tag team duo of NZ on Air and TVNZ,
-        who both recognise the importance of embracing our cultural identity and
-        developing kids’ curiosity and knowledge, particularly at a young age.
-        We know a brighter, more colourful future starts with our tamariki. The
-        more confident and accepting our children are, the better our nation can
-        become. HEIHEI is the home of content made by some of our best producers
-        and storytellers from all across New Zealand. They are committed to
-        telling New Zealand stories that will spark children’s imaginations and
-        we’re proud to feature their work on HEIHEI.
-      </p>
-      <h2>What is HEIHEI?</h2>
-      <p>
-        Imagine a place where our tamariki can see and hear themselves
-        celebrated. A place that invites their culture, communities, life
-        experiences and accents to be reflected back at them. HEIHEI is a bright
-        and cheerful streaming platform filled exclusively with safe,
-        age-appropriate content. It is packed full of stuff that speaks to those
-        young at heart — a growing, organised catalogue of locally-made and
-        handpicked international shows, stories, games and music. We also hold
-        exciting competitions, with great prizes to be won. Through HEIHEI, our
-        kids will learn. They will enjoy. And they will grow It’s free and
-        ad-free. Yes, you read that right. You can start watching, listening and
-        playing now!
-      </p>
+      <About>
+        <H1>Contact Us</H1>
+        <H2>Want to get in touch?</H2>
 
-      <h2>Why HEIHEI?</h2>
-      <p>
-        Shouldn’t the question be, “Why not?” Why shouldn’t our children have
-        access to diverse local media made specifically for them? Stories are
-        vital for the development of identity and imagination. Stories relevant
-        to their age and culture should be available to all New Zealand
-        children. Children of all backgrounds, ethnicities and abilities should
-        see and hear themselves reflected positively in local content. Plus,
-        it’s only fair our kids get a turn, right? This is why handing them a
-        digital content menu — filled with appetising, age-appropriate
-        selections for their eyeballs and brains — makes sense. And who knows —
-        you might like the stuff on HEIHEI just as much as them! Also, our
-        platform is completely safe and you don’t have to worry about them
-        somehow unlocking codes and watching stuff meant only for adults. So sit
-        down and enjoy HEIHEI: a reliable place to satisfy your child’s hunger
-        for quality digital screen entertainment.
-      </p>
+        <P>
+          Email Us heihei@tvnz.co.nz We always love hearing from our wonderful
+          community, but if you have a question make sure to check out Help &
+          FAQs first — you may find the answer to your question there.
+        </P>
+      </About>
     </>
   )
 }
