49dbfd3c0e3a6ba0496eb1fabe27a75aab05bf02
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2024-04-10 11:56:45 +1200

    changed about us to NZGDA

--- src/app/(components)/footer.tsx
+++ src/app/(components)/footer.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import Image from 'next/image'
 import bannerBottom from '../../../public/images/footer-side.png'
+import nzonair from '../../../public/images/nzonair-logo.png'
+import nzgda from '../../../public/images/NZGDA.png'
 import Link from 'next/link'
 
 export default function Footer() {
@@ -12,15 +14,17 @@ export default function Footer() {
           <Image quality={100} src={bannerBottom} alt={'Background'}></Image>
         </div>
         <div className="absolute w-[35%] sm:w-44 left-[50%] -translate-x-1/2 translate-y-1/4">
-          <div className="flex justify-center gap-[10%] *:text-mainred *:font-semibold *:text-xl hover:*:underline hover:*:cursor-pointer">
-            {/* <div>
-              <Image quality={100} src={tvnz} alt={'Background'}></Image>
+          <Link
+            href={'/about'}
+            className="flex justify-center hover:scale-105 duration-100 items-center gap-[10%] *:text-mainred *:font-semibold *:text-xl hover:*:underline hover:*:cursor-pointer"
+          >
+            <div>
+              <Image quality={100} src={nzgda} alt={'Background'}></Image>
             </div>
             <div>
               <Image quality={100} src={nzonair} alt={'Background'}></Image>
-            </div> */}
-            <Link href={'/about'}>About Us</Link>
-          </div>
+            </div>
+          </Link>
         </div>
         <div className="w-[40%]  translate-y-1/3 scale-125">
           <Image quality={100} src={bannerBottom} alt={'Background'}></Image>
