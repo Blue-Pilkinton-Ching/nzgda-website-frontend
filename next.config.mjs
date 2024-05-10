4f84552e0ed40af515770a2efb6eb976d60c6be3
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2024-05-10 13:06:02 +1200

    update stuff

--- next.config.mjs
+++ next.config.mjs
@@ -23,6 +23,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'nzgda-website.s3.ap-southeast-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 }
