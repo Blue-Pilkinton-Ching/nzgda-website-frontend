4f8ef324e4ac2fb06836070517f439ba55e2c2ff
Blue <whatsup@bluepc.me>
2025-06-02 15:53:45 +1200

    added something to next config

--- next.config.mjs
+++ next.config.mjs
@@ -37,7 +37,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'heihei-bucket.syd1.digitaloceanspaces.com',
+        hostname: 'heihei-bucket.syd1.cdn.digitaloceanspaces.com',
         port: '',
         pathname: '/**',
       },
